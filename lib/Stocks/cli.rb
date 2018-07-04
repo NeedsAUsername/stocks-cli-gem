4f033e8aa7cd7157a72c7870043d1c05e9975852
Kevin Chi <Kevin@Kevins-Air.fios-router.home>
2018-07-04 13:03:31 -0400

    added to menu and created more methods

--- lib/Stocks/cli.rb
+++ lib/Stocks/cli.rb
@@ -8,6 +8,7 @@ class Stocks::CLI
     end
 
     def show_indices
+        puts "Here is how the market is doing today"
         puts <<-DOC.gsub /^\s*/, ''
             1. SPY - 2713.22
             2. DOW - 24,174.82
@@ -19,6 +20,29 @@ class Stocks::CLI
     end
 
     def menu
+        puts "What ticker would you like info or price on?"
+        input = nil
+        while input != "exit"
+            puts "Type ticker-info for detailed info, and type ticker-price for current price, and type exit to exit."
+            input = gets.strip
+            if input.end_with?("info")
+                show_info
+            elsif input.end_with?("price")
+                show_price
+            elsif input == "exit"
+                puts "Exiting"
+            else
+                puts "Try again."
+            end
+        end
+    end
+
+    def show_info
+        puts "ticker info"
+    end
+
+    def show_price
+        puts "ticker price"
     end
 
 end
