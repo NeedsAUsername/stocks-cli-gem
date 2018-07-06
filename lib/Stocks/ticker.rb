1ff0580bf445a2ae2648c0a0e30095164cfe3f29
Kevin Chi <Kevin@Kevins-Air.fios-router.home>
2018-07-05 22:36:57 -0400

    added news functionality

--- lib/Stocks/ticker.rb
+++ lib/Stocks/ticker.rb
@@ -1,10 +1,11 @@
 class Stocks::Ticker
 
-    attr_accessor :url, :html, :name, :price, :price_change, :info
+    attr_accessor :url, :html, :name, :price, :price_change, :info, :news, :links
 
-    def initialize(ticker) #add symbol as parameter later
+    def initialize(ticker)
         @url = "https://finance.yahoo.com/quote/#{ticker.upcase}"
-        #later make url change with symbol
+        @links = []
+        @number_of_articles = 0
     end
 
     def scrape
@@ -13,6 +14,7 @@ class Stocks::Ticker
         self.price = self.html.css("#quote-header-info span")[1].text
         self.price_change = self.html.css("#quote-header-info span")[2].text
         self.info = self.html.css("#quote-summary div")[1].text
+        self.news = self.html.css("#quoteNewsStream-0-Stream a")
     end
 
     def show_info
@@ -21,7 +23,24 @@ class Stocks::Ticker
         puts self.price
         puts self.price_change
         puts self.info
+        puts "Latest News:"
+        self.news.each_with_index do |title, index|
+            puts "#{index + 1}. #{title.text}"
+            self.links << title.attribute("href").value
+        end
+        puts "Do you want to view any of these articles? Type its number or else type no"
+        input = nil
+        while input != "no"
+            input= gets.strip
+            if input == "no"
+                puts "exiting to main menu"
+            elsif input.to_i <= self.links.length && input.to_i > 0 #strings will convert to 0
+                system("open https://finance.yahoo.com#{self.links[input.to_i - 1]}")
+            else puts "That is not a valid article."
+            end
+        end
     end
+
 
     def show_price
         self.scrape
