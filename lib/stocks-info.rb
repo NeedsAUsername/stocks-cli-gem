3f563b1fe6bd6a6207830eb7ca06b5851e6267de
NeedsAUsername <kevinchi118@gmail.com>
2018-10-21 22:39:55 -0400

    trying syntax change

--- lib/stocks-info.rb
+++ lib/stocks-info.rb
@@ -1,8 +1,8 @@
 require "nokogiri"
 require "open-uri"
 
-require_relative "./stocks-info/version"
-require_relative "./stocks-info/cli"
-require_relative "./stocks-info/ticker"
-require_relative "./stocks-info/stocktwits"
-require_relative "./stocks-info/scraper"
+require_relative "stocks-info/version"
+require_relative "stocks-info/cli"
+require_relative "stocks-info/ticker"
+require_relative "stocks-info/stocktwits"
+require_relative "stocks-info/scraper"
